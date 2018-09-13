ad1dc1ddadc0c1503e915f42b7aefc67f3f400d4
fiahfy <d.fiahfy@gmail.com>
2018-09-13 14:28:25 +0900

    Upgrade dev dependencies and use rollup

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import formatter from '../src'
+import formatter from '../lib'
 
 describe('darwin', () => {
   beforeAll(() => {
@@ -7,8 +7,45 @@ describe('darwin', () => {
     })
   })
 
-  test('equal', () => {
-    expect(formatter('CmdOrCtrl+A')).toBe('⌘A')
+  test('Format', () => {
+    expect(formatter('A')).toBe('A')
+    expect(formatter('Plus')).toBe('+')
+    expect(formatter('Space')).toBe('Space')
+    expect(formatter('Tab')).toBe('⇥')
+    expect(formatter('Backspace')).toBe('⌫')
+    expect(formatter('Delete')).toBe('⌦')
+    expect(formatter('Return')).toBe('↩')
+    expect(formatter('Enter')).toBe('↩')
+    expect(formatter('Up')).toBe('↑')
+    expect(formatter('Down')).toBe('↓')
+    expect(formatter('Left')).toBe('←')
+    expect(formatter('Right')).toBe('→')
+    expect(formatter('PageUp')).toBe('⇞')
+    expect(formatter('PageDown')).toBe('⇟')
+    expect(formatter('Escape')).toBe('⎋')
+    expect(formatter('Esc')).toBe('⎋')
+  })
+
+  test('Alias', () => {
+    expect(formatter('Cmd+A')).toBe(formatter('Command+A'))
+    expect(formatter('CmdOrCtrl+A')).toBe(formatter('CommandOrControl+A'))
+    expect(formatter('Alt+A')).toBe(formatter('Option+A'))
+    expect(formatter('Alt+A')).toBe(formatter('AltGr+A'))
+  })
+
+  test('Modifier', () => {
+    expect(formatter('Ctrl+A')).toBe('⌃A')
+    expect(formatter('Alt+A')).toBe('⌥A')
+    expect(formatter('Shift+A')).toBe('⇧A')
+    expect(formatter('Cmd+A')).toBe('⌘A')
+  })
+
+  test('Modifier Order', () => {
+    expect(formatter('A+Cmd')).toBe('⌘A')
+    expect(formatter('Ctrl+Alt+Shift+Cmd+A')).toBe('⌃⌥⇧⌘A')
+    expect(formatter('Cmd+Ctrl+Alt+Shift+A')).toBe('⌃⌥⇧⌘A')
+    expect(formatter('Shift+Cmd+Ctrl+Alt+A')).toBe('⌃⌥⇧⌘A')
+    expect(formatter('Alt+Shift+Cmd+Ctrl+A')).toBe('⌃⌥⇧⌘A')
   })
 })
 
@@ -19,7 +56,7 @@ describe('win32', () => {
     })
   })
 
-  test('equal', () => {
-    expect(formatter('CmdOrCtrl+A')).toBe('Ctrl+A')
-  })
+  // test('equal', () => {
+  //   expect(formatter('CmdOrCtrl+A')).toBe('Ctrl+A')
+  // })
 })
