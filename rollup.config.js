975eab832c747fc60567b4383688b765ca2d17ca
fiahfy <d.fiahfy@gmail.com>
2018-09-19 23:47:42 +0900

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,17 @@ import babel from 'rollup-plugin-babel'
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'index.js',
+  output: [{
+    file: 'dist/index.js',
     format: 'cjs'
-  },
+  }, {
+    file: 'dist/index.esm.js',
+    format: 'esm'
+  }, {
+    file: 'dist/index.umd.js',
+    format: 'umd',
+    name: 'electronAcceleratorFormatter'
+  }],
   plugins: [
     babel({
       exclude: 'node_modules/**'
